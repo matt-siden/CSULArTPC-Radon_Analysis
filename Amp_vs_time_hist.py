23ec56e57a65a56ddde2a125946f04c5150a499b
matt-siden <msiden13@gmail.com>
2024-09-03 18:27:56 +0200

    changes to 2d hist

--- Amp_vs_time_hist.py
+++ Amp_vs_time_hist.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import funcs
-
+from matplotlib.colors import LogNorm
 
 # From each csv file pulls 
 #     1) The file creation timestamp
@@ -21,30 +21,40 @@ def process_csv(csv_path):
 # y_list - list of y data
 # in_file - pass filename - used to make title
 def plot_2D_hists(x_list, y_list, in_file):
-    fig, ax = plt.subplots(1,2, figsize=(16,8))
-   
-    #num_subdirs = len([x[0] for x in os.walk(in_file)])
-    #print(num_subdirs)
-    log_hist_title = os.path.basename(os.path.normpath(in_file)) + " Histogram - Log Scale"
+    fig, ax = plt.subplots(1, 2, figsize=(16, 8))
+
+    # Titles for the subplots
+    log_hist_title = os.path.basename(os.path.normpath(in_file)) + " Histogram - Log Color Scale"
     reg_hist_title = os.path.basename(os.path.normpath(in_file)) + " Histogram"
 
+    # Determine range for the histograms
     xmin = np.min(x_list)
     xmax = np.max(x_list)
 
-    #2 minute bin Width I think
-    num_bins_x = int((xmax - xmin) / 2)
+    ymin = np.min(y_list)
+    ymax = np.max(y_list)
 
-    ax[0].hist2d(x_list, y_list, bins=[num_bins_x,500])
-    ax[0].set_ylim([-1,0])
+    # Bin calculations
+    num_bins_x = int((xmax - xmin) / 2)
+    num_bins_reg_y = int(-(0.1 / 0.0001) * ymin)
+
+    # Regular 2D Histogram
+    h1 = ax[0].hist2d(x_list, y_list, bins=[num_bins_x, num_bins_reg_y])
+    ax[0].set_ylim([-0.2, 0])
     ax[0].set_title(reg_hist_title, fontsize=20)
     ax[0].set_xlabel("Minutes Since Run Start", fontsize=16)
     ax[0].set_ylabel("Waveform Amplitude", fontsize=16)
 
-    ax[1].hist2d(x_list, y_list, bins=[num_bins_x,100])
-    ax[1].set_yscale('symlog')
+    # 2D Histogram with Logarithmic Color Scale
+    h2 = ax[1].hist2d(x_list, y_list, bins=[num_bins_x, 100], norm=LogNorm())
+    ax[1].set_ylim([-0.2, 0])
     ax[1].set_title(log_hist_title, fontsize=20)
     ax[1].set_xlabel("Minutes Since Run Start", fontsize=16)
-    ax[1].set_ylabel("Waveform Amplitude",fontsize=16)
+    ax[1].set_ylabel("Waveform Amplitude", fontsize=16)
+
+    # Add colorbars
+    fig.colorbar(h1[3], ax=ax[0])
+    fig.colorbar(h2[3], ax=ax[1])
 
     plt.show()
 
